6785d25055d0eb8e79e012a2682df9116eff5f43
DanielMontesDeOca <daniel_ernesto64@hotmail.com>
2017-03-03 17:40:45 -0600

    Updated invoices startup scripts

--- services/invoices/scripts/startup.integration.sh
+++ services/invoices/scripts/startup.integration.sh
@@ -2,18 +2,6 @@
 
 set -u
 
-until nc -z -v -w30 rabbitmq 5672
-do
-  echo "Waiting for rabbitmq to start..."
-  sleep 5
-done
-
-until nc -z -v -w30 db 5432
-do
-  echo "Waiting for rabbitmq to start..."
-  sleep 5
-done
-
 # shellcheck disable=SC1091
 source /var/lib/core/database/postgres-create.sh
 
