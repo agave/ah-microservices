7ad9ea721ae6e35010e2a010a42b0cba050776ad
gypsydiver <fernando@agavelab.com>
2017-03-06 11:31:41 -0600

    add logger

--- services/users/main.go
+++ services/users/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+func init() {
+	configureLogger()
+}
+
+func configureLogger() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.InfoLevel)
+}
 
 func main() {
-	fmt.Println("Awesomesauce")
+	log.Info("Awesomesauce")
 }
