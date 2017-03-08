be55d1e3e80da2c167ec0fb6972c812b9128750a
F <fernando@agavelab.com>
2017-03-07 19:09:39 -0600

    test mode fro user service, db migrations included

--- services/users/scripts/startup.test.sh
+++ services/users/scripts/startup.test.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-set -x -u
+set -ex
+
+# Create Database
+# shellcheck disable=SC1091
+source /home/database/postgres-create.sh
 
 apk add -U --no-progress curl g++ gcc git make
+
+# Run migrations
+go get github.com/mattes/migrate
+
+DB_URL=postgres://"$DB_USER":"$DB_PASSWORD"@"$DB_HOST":"$DB_PORT"?sslmode=disable
+
+cd "$GOPATH/src/github.com/agave/ah-microservices/services/users" \
+    && migrate -url "$DB_URL" -path ./migrations up
 
 cd "$GOPATH/src/github.com/agave/ah-microservices/services/users" && make test
 
 apk del --no-progress curl g++ gcc git make
+
+cd "$GOPATH/src/github.com/agave/ah-microservices/services/users" \
+    && migrate -url "$DB_URL" -path ./migrations down
