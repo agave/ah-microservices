70eb551c404a7183d1ed148d1fb55f58c8c33e22
F <fernando@agavelab.com>
2017-03-07 19:10:37 -0600

    shellcheck

--- services/users/scripts/proto-gen.sh
+++ services/users/scripts/proto-gen.sh
@@ -5,5 +5,5 @@ set -x -e;
 IMPORT_PATH=$1
 SERVICE_PROTO=$2
 
-protoc -I $IMPORT_PATH "$IMPORT_PATH/$SERVICE_PROTO.proto" \
+protoc -I "$IMPORT_PATH" "$IMPORT_PATH/$SERVICE_PROTO.proto" \
 	--go_out=plugins=grpc:"$GOPATH/src"
