7923607fb1a32e179c31e81ceaa50ffb4b961fc2
DanielMontesDeOca <daniel_ernesto64@hotmail.com>
2017-03-09 16:07:04 -0600

    test

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 OS=$(uname -a | cut -d ' ' -f 1)
-interface="eth0"
 
 if [ "$OS" == "Darwin" ]; then
-  interface="en0"
+  # shellcheck disable=SC2005
+  # shellcheck disable=SC2155
+  export DOCKER_HOST_IP="$(ifconfig en0 | grep "inet " | cut -d ' ' -f 2)"
+else
+  # shellcheck disable=SC2005
+  # shellcheck disable=SC2155
+  export DOCKER_HOST_IP="$(ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 fi
-
-echo "DOCKER_HOST_IP"
-# shellcheck disable=SC2005
-# shellcheck disable=SC2155
-echo "$(ifconfig $interface | grep "inet " | cut -d ' ' -f 2)"
-# shellcheck disable=SC2005
-# shellcheck disable=SC2155
-export DOCKER_HOST_IP="$(ifconfig $interface | grep "inet " | cut -d ' ' -f 2)"
