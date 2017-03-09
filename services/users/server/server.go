ce5c0d26d12d17d7d4e4b62bb96cf863d242e28c
F <fernando@agavelab.com>
2017-03-09 16:36:10 -0600

    add get user + functional tests

--- services/users/server/server.go
+++ services/users/server/server.go
@@ -18,12 +18,37 @@ type userServer struct{}
 
 // GetUser looks for the Id param in the database and returns user details if found
 func (s *userServer) GetUser(ctx xContext.Context, id *userGen.Id) (*userGen.Profile, error) {
-	return nil, nil
+	log.WithFields(log.Fields{
+		"GUID": id.GetGuid(), "data": id.String(),
+	}).Info("Received request")
+
+	log.WithField("GUID", id.GetGuid()).Info("Searching for user in db")
+
+	u := user.Users{ID: id.GetId()}
+	h, err := db.Engine.Get(&u)
+	if err != nil {
+		log.WithField("GUID", id.GetGuid()).Info("Internal DB Error")
+		return &userGen.Profile{}, grpc.Errorf(codes.Internal, "%s", err.Error())
+	}
+
+	if h {
+		log.WithField("GUID", id.GetGuid()).Info("Found")
+		p := userGen.Profile(u)
+		return &p, nil
+	}
+
+	log.WithField("GUID", id.GetGuid()).Info("Not Found")
+	return &userGen.Profile{}, grpc.Errorf(codes.NotFound, "Not Found")
 }
 
 // CreateUser creates a user in the database based on the Create param
 func (s *userServer) CreateUser(ctx xContext.Context, c *userGen.Create) (*userGen.Profile, error) {
-	//TODO: email/security validation
+	//TODO: email format/security validation
+	if c.GetEmail() == "" {
+		return &userGen.Profile{},
+			grpc.Errorf(codes.InvalidArgument, "Email can't be empty")
+	}
+
 	if c.Balance < 0 {
 		return &userGen.Profile{},
 			grpc.Errorf(codes.InvalidArgument, "Balance can't be negative")
@@ -31,10 +56,11 @@ func (s *userServer) CreateUser(ctx xContext.Context, c *userGen.Create) (*userG
 
 	user := user.Users{Email: c.GetEmail()}
 
+	log.WithField("userEmail", user.Email).Info("Looking for user in db")
 	exists, err := db.Engine.Get(&user)
 	if err != nil {
 		return &userGen.Profile{},
-			grpc.Errorf(codes.Internal, "%s", err.Error())
+			grpc.Errorf(codes.Unknown, "Unable to create user")
 	}
 
 	if exists {
