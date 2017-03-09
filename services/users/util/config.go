ce15d882173501243d2ad310d6d2a316f21e8976
F <fernando@agavelab.com>
2017-03-08 19:14:40 -0600

    db connection

--- services/users/util/config.go
+++ services/users/util/config.go
@@ -1,10 +1,22 @@
 package util
 
+// DBOptions holds database connection details
+type DBOptions struct {
+	Host     string   `yaml:"DBHost"`
+	Port     int      `yaml:"DBPort"`
+	DBName   string   `yaml:"DBName"`
+	Username string   `yaml:"DBUser"`
+	Password string   `yaml:"DBPassword"`
+	Options  []string `yaml:"DBOptions"`
+}
+
 // Configuration defines all runtime variables
 type Configuration struct {
-	KafkaBrokers []string `yaml:"KafkaBrokers"`
-	Topics       []string `yaml:"KafkaTopics"`
-	GroupID      string   `yaml:"ConsumerGroupID"`
+	KafkaBrokers []string  `yaml:"KafkaBrokers"`
+	Topics       []string  `yaml:"KafkaTopics"`
+	GroupID      string    `yaml:"ConsumerGroupID"`
+	DB           DBOptions `yaml:"DB"`
+	Port         int       `yaml:"Port"`
 }
 
 // Config is a Configuration instance with predefined defaults
