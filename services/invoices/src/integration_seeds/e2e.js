1801662acb1206c109b7e30931806e5c5d42552b
Daniel Montes de Oca <daniel_ernesto64@hotmail.com>
2017-03-21 11:55:05 -0600

    Renamed invoices fixture name

--- services/invoices/src/integration_seeds/e2e.js
+++ services/invoices/src/integration_seeds/e2e.js
@@ -1,4 +1,4 @@
 const { Invoice } = require('../models');
-const data = require('/var/lib/core/database/integration_fixtures/invoice.json');
+const data = require('/var/lib/core/database/integration_fixtures/invoices.json');
 
 Invoice.bulkCreate(data);
